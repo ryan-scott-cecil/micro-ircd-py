c2ca52f542953b0a73ded61bbd4f7f3f7ba8f564
ryan-scott-cecil <ryan.cecil@gmail.com>
2024-02-17 19:42:25 -0800

    you can see data from client in terminal now... pretty useless tbh.

--- main.py
+++ main.py
@@ -1,47 +1,19 @@
+from server import Server
+
+
 import socket
 import threading
 
 def main():
     server = Server()
-    server.start()
+    fqdn = socket.getfqdn()
+    print(f" fqdn is: {fqdn}")
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        server.print_error("Server Interrupted by sysadmin")
 
     return print('working')
 
-class Server:
-    def __init__(self, host='localhost', port=6697):
-        self.host = host
-        self.port = port
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((self.host, self.port))
-        self.server.listen()
-
-    def start(self):
-        print(f"Server is listening on {self.host}:{self.port}")
-        while True:
-            client_socket, client_address = self.server.accept()
-            print(f"Accepted connection from {client_address}")
-            threading.Thread(target=self.handle_client, args=(client_socket,)).start()
-
-    def handle_client(self, client):
-        while True:
-            try:
-                message = client.recv(2048).decode('utf-8')
-                if not message:
-                    break
-        
-                # Parse the message and extract the command and parameters
-                parts = message.strip().split(' ')
-                command = parts[0]
-                params = parts[1:]
-            
-                if command == 'PRIVMSG':
-                    target = params[0]
-                    message = ' '.join(params[1:])
-                    # Respond to the PRIVMSG command
-                    client.send(f":{self.host} PRIVMSG {target}:{message}\n".encode('utf-8'))
-            except Exception as e:
-                print(f"Error handling client: {e}")
-                break
-
-
-main()
+if __name__ == "__main__":
+    main()
